1bed6d4abff5bee18523f19a989739c5cea5b9cf
uamii <umayarhossainashik48889@gmail.com>
2024-08-02 11:45:00 +0600

    Ready to upload

---
+++ function.js
@@ -0,0 +1,182 @@
+let count=0;
+let freq={
+    'A1':0,
+    'A2':0,
+    'A3':0,
+    'A4':0,
+    'B1':0,
+    'B2':0,
+    'B3':0,
+    'B4':0,
+    'C1':0,
+    'C2':0,
+    'C3':0,
+    'C4':0,
+    'D1':0,
+    'D2':0,
+    'D3':0,
+    'D4':0,
+    'E1':0,
+    'E2':0,
+    'E3':0,
+    'E4':0,
+    'F1':0,
+    'F2':0,
+    'F3':0,
+    'F4':0,
+    'G1':0,
+    'G2':0,
+    'G3':0,
+    'G4':0,
+    'H1':0,
+    'H2':0,
+    'H3':0,
+    'H4':0,
+    'I1':0,
+    'I2':0,
+    'I3':0,
+    'I4':0,
+    'J1':0,
+    'J2':0,
+    'J3':0,
+    'J4':0,
+
+}
+let cnt=parseInt(0);
+const AllBtn=document.getElementsByClassName('button');
+for(const btn of AllBtn)
+{
+    btn.addEventListener('click',function handler(event)
+    {
+
+        
+        const section=event.target.innerText;
+        freq[section]++;
+        if(freq[section]===1)
+        {
+            count++;
+        }
+        
+        const seatType="economy";
+        const price=550;
+        if(count>=4)
+        {
+            document.getElementById('apply').removeAttribute('disabled',true);
+            document.getElementById('applyID').removeAttribute('disabled',true);
+        }
+
+        if(count<=4)
+        {
+            // disable button
+
+            if(freq[section]>=1)
+            {
+                document.getElementById('pas').removeAttribute('disabled',true);
+                document.getElementById('nub').removeAttribute('disabled',true);
+                document.getElementById('em').removeAttribute('disabled',true);
+            }
+            document.getElementById('pas').addEventListener('input',function(event)
+            {
+                if(event.target.value.length>3)
+                {
+                    cnt+=1;
+                    checkCnt();
+
+                }
+                
+                
+            });
+            document.getElementById('nub').addEventListener('input',function(event)
+            {
+                
+                if(event.target.value.length>=9)
+                {
+                    cnt+=1;
+                    checkCnt();
+                }
+                
+            });
+            
+            document.getElementById('em').addEventListener('input',function(event)
+            {
+                if(event.target.value.length>=14)
+                {
+                    cnt+=1;
+                    checkCnt();
+                }
+                
+            });
+            if(freq[section]===1)
+            {
+                setbgColor(event);
+                settextColor(event);
+                // make button visited if it once used
+                
+                // end her
+                classSeat('seatTwist',count);
+                showClassSeat('appendSide',section,seatType,price);
+                calTotal('total-cost',price);
+                GrandTotal('g-total',price);
+                availableSeat('setAvailable');
+    
+                document.getElementById('apply').addEventListener('input',function m(e)
+                {
+                    const input=e.target.value;
+                    let offer='NEW15';
+                    let offer1='Couple 20';
+                    let discountOFFER1=15;
+                    let discountOFFER2=20;
+                    
+
+                    document.getElementById('applyID').addEventListener('click',function name()
+                    {
+                        if((input)===(offer))
+                        {
+                            discountThePrice('g-total',discountOFFER1);
+                            document.getElementById('distract').setAttribute('disabled',true);
+                            document.getElementById('apply').setAttribute('disabled',true);
+                            document.getElementById('applyID').setAttribute('disabled',true);
+                            
+
+                        }
+                        else if((input)===(offer1)){
+                            discountThePrice('g-total',discountOFFER2);
+                            document.getElementById('distract').setAttribute('disabled',true);
+                            document.getElementById('apply').setAttribute('disabled',true);
+                            document.getElementById('applyID').setAttribute('disabled',true);
+                        }
+                        else
+                        {
+                            // console.log('not matched');
+                        }
+                    })
+                    
+                })
+            }
+        }
+        else
+        {
+            if(count===5)
+            {
+                
+                alert('You selected more than 4 which is not allow');
+        
+    
+            }
+        }
+        // need discount also which is 15 and its has coupon code if matched
+        /*
+            step-1=>if once visit a button make it true
+            step-2=>make button disabled if once used
+            event.target.setAttribute("disabled", true);
+             
+        */
+    })
+}
+
+function checkCnt() {
+    if (cnt === 3) {
+        document.getElementById('applied').removeAttribute('disabled', true);
+    }
+}
+
---
+++ functionProgram.js
@@ -0,0 +1,79 @@
+//set background&Text color
+// function   clickOnSeatA3 (){
+//     removebgClour   ('A3')
+//     setbgcolor      ('A3')
+//     removeTxtClour  ('A3')
+//     setTxtColor     ('A3')
+    
+// }
+
+
+function settextColor (event)
+{
+    const setColor=event.target.classList.add('text-white');
+    
+}
+function setbgColor(event)
+{
+   
+    const setColor=event.target.classList.add('bg-[#1DD100]');
+}
+
+// update available seat
+function availableSeat(elementId)
+{
+    const AvailableSeat=document.getElementById(elementId);
+    const presentSeat=AvailableSeat.innerText;
+    document.getElementById(elementId).innerText=parseInt(presentSeat)-1;
+}
+// create li and append some text inside it
+function showClassSeat(elementId,section,seatType,price)
+{
+    const element=document.getElementById(elementId);
+    const div=document.createElement('div');
+    div.classList.add('grid');
+    div.classList.add('grid-cols-3');
+    const p=document.createElement('p');
+    p.innerText=section;
+    const p2=document.createElement('p');
+    p2.innerText=seatType;
+    const p3=document.createElement('p');
+    p3.innerText=price;
+    div.appendChild(p);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    element.appendChild(div);
+
+}
+function calTotal(elementId,price)
+{
+    const total=document.getElementById(elementId);
+    let cost=parseInt(total.innerText)+parseInt(price);
+    document.getElementById(elementId).innerText=parseInt(cost);
+}
+function GrandTotal(elementId,price)
+{
+    const element=document.getElementById(elementId).innerText;
+    let elementCost=parseInt(element)+parseInt(price);
+    document.getElementById(elementId).innerText=parseInt(elementCost);
+}
+// Discount for NEW15 AND COUPLE20
+let discountApplied=false;
+function discountThePrice(elementId,APPLY)
+{
+    if(!discountApplied)
+    {
+        const element=document.getElementById(elementId);
+        const originalPrice=parseInt(element.innerText);
+        const disCounterPRICE=originalPrice*(APPLY/100);
+        const finalDiscount=originalPrice-disCounterPRICE;
+        document.getElementById('g-total').innerText=parseInt(finalDiscount);
+        discountApplied=true;
+    }
+}
+// function countClassSeat
+function classSeat(elementId,count)
+{
+    const element=document.getElementById('seatTwist');
+    element.innerText=count;
+}
